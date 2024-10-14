a8287b466e78e510a3adf8ef4ffe63ef6503feea
Dalyan Parker <dalyan.kyle.parker@gmail.com>
2024-10-14 20:22:01 +0100

    add aws repos (#1)
    Co-authored-by: Dalyan Parker <dalyan@quiver.co.uk>

--- locals.tf
+++ locals.tf
@@ -1,18 +1,35 @@
 locals {
-    repos = {
-        "fem-eci-terraform-tfe" = {
-            description        = "Automation for Terraform Enterprise"
-            gitignore_template = "Terraform"
-            name               = "fem-eci-terraform-tfe"
-            topics             = ["terraform"]
-            visibility         = "public"
-        }
-        "fem-eci-terraform-github" = {
-            description        = "Automation for GitHub"
-            gitignore_template = "Terraform"
-            name               = "fem-eci-terraform-github"
-            topics             = ["terraform"]
-            visibility         = "public"
-        }
+  repos = {
+    "fem-eci-terraform-tfe" = {
+      description        = "Automation for Terraform Enterprise"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-tfe"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
     }
+
+    "fem-eci-terraform-github" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-github"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+
+    "fem-eci-terraform-aws-network" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-aws-network"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+
+    "fem-eci-terraform-aws-cluster" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-aws-cluster"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+  }
 }
